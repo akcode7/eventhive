4bd14cdef32f44a2c0685c43df8cc227d3c898ed
akcode7 <ashishgotapogo10@gmail.com>
2024-12-05 23:48:40 +0530

    your event page ui fixing

--- your-events.php
+++ your-events.php
@@ -35,38 +35,43 @@ if (isset($_SESSION['username'])) {
         </style>
     </head>
 <body>
-    <div class="container p-5">
-        <h1 class="py-3 text-bold text-xl">Your Events</h1>
-        <ul class="max-w-md divide-y divide-gray-200 ">
-        <?php
-    $sql = mysqli_query($conn, "SELECT * FROM `event_detail` WHERE  `user_name` = '$username'");
-    while($row = mysqli_fetch_assoc($sql)){
-?>
-            <li class="pb-3 sm:pb-4">
-               <div class="flex items-center space-x-4 ">
-                  <div class="flex-shrink-0">
-                     <img class="w-8 h-8 rounded-full" src="src/images/university.png" alt="Neil image">
-                  </div>
-                  <div class="flex-1 min-w-0">
-                     <p class="text-sm font-medium text-gray-900 truncate">
-                     <?php echo $row['event_name']?>
-                     </p>
-                     <p class="text-sm text-gray-500 truncate ">
-                     <?php echo $row['event_date']?>
-                     </p>
-                  </div>
-                  <div class="inline-flex items-center text-base font-semibold text-gray-900">
-                    <a href="edit-event.php?id=<?php echo urlencode($row['event_id']); ?>" class="text-lg font-bold text-teal-500 px-2 cursor-pointer">Edit</a>
-                    <a href="event-details.php?id=<?php echo urlencode($row['event_id']); ?>" class="text-lg font-bold text-blue-500 px-2 cursor-pointer">View</a>
-                  </div>
-               </div>
-            </li>
+    <!-- Header starts -->
+  <?php include 'src/component/header.php';?>
+    <!-- Header ends -->
+    <div class="container mx-auto px-4 py-3">
+        <h1 class="py-3 text-bold text-2xl text-center">Your Events</h1>
+        <div class="flex justify-center items-center mx-auto pt-8">
+            <ul class="max-w-5xl divide-y divide-gray-200 shadow-lg border p-5 w-full">
+                <?php
+                    $sql = mysqli_query($conn, "SELECT * FROM `event_detail` WHERE  `user_name` = '$username'");
+                    while($row = mysqli_fetch_assoc($sql)){
+                ?>
+                <li class="pb-3 sm:pb-4">
+                    <div class="flex items-center space-x-4 py-3">
+                        <div class="flex-shrink-0">
+                            <img class="w-28 md:w-40 rounded-md" src="<?php echo $row['event_img']?>" alt="Neil image">
+                        </div>
+                        <div class="flex-1 min-w-0">
+                            <p class="text-sm font-medium text-gray-900 truncate">
+                                <?php echo $row['event_name']?>
+                            </p>
+                            <p class="text-sm text-gray-500 truncate ">
+                                <?php echo $row['event_date']?>
+                            </p>
+                        </div>
+                        <div class="inline-flex items-center text-base font-semibold text-gray-900">
+                            <a href="edit-event.php?id=<?php echo urlencode($row['event_id']); ?>" class="text-lg font-bold text-teal-500 px-2 cursor-pointer">Edit</a>
+                            <a href="event-details.php?id=<?php echo urlencode($row['event_id']); ?>" class="text-lg font-bold text-blue-500 px-2 cursor-pointer">View</a>
+                        </div>
+                    </div>
+                </li>
 
-            <?php
-  }
-  ?>
-            
-        </ul>
+                <?php
+                    }
+                ?>
+            </ul>
+        </div>
+
     </div>
    
 </body>
